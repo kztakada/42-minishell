0a1f7ef743c42bf6a496e8e6e4e208814cb4ad74
42kharuya <haruya.0411.k@gmail.com>
2025-04-06 03:03:33 +0900

    Add built-in commands: cd, echo, env, exit, export, pwd, unset

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,38 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins_main.c                                    :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/03 21:53:10 by kharuya           #+#    #+#             */
-/*   Updated: 2025/04/03 21:53:11 by kharuya          ###   ########.fr       */
+/*   Created: 2025/04/03 21:53:15 by kharuya           #+#    #+#             */
+/*   Updated: 2025/04/03 21:53:17 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/h_minishell.h"
 
-#ifndef TEST
-
-int	main(int argc, char *argv[])
+int	ft_echo(char **args)
 {
-	(void)argc;
-	(void)argv;
+	int		i;
+	bool	n_flag;
 
-	// // pwdコマンド
-	// ft_pwd();
-	// return (0);
-
-	// // echoコマンド
-	// // normal case
-	// char *test_1[] = {"echo", "-n", "Hello", "World", NULL};
-	// ft_echo(test_1);
-	// // n_flag case
-	// char *test_2[] = {"echo", "-n", "-n", NULL};
-	// ft_echo(test_2);
-	// // complex case
-	// char *test_3[] = {"echo", "-n", "-n", "-nHello", "-n", NULL};
-	// ft_echo(test_3);
+	i = 1;
+	n_flag = false;
+	while (ft_strcmp(args[i], "-n") == 0)
+	{
+		n_flag = true;
+		i++;
+		if (args[i] == NULL)
+			return (0);
+	}
+	while (args[i] != NULL)
+	{
+		printf("%s", args[i++]);
+		if (args[i] != NULL)
+			printf(" ");
+	}
+	if (!n_flag)
+		printf("\n");
 	return (0);
 }
-#endif // TEST
--- builtins/export.c
+++ builtins/export.c
@@ -1,38 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins_main.c                                    :+:      :+:    :+:   */
+/*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/03 21:53:10 by kharuya           #+#    #+#             */
-/*   Updated: 2025/04/03 21:53:11 by kharuya          ###   ########.fr       */
+/*   Created: 2025/04/03 21:53:37 by kharuya           #+#    #+#             */
+/*   Updated: 2025/04/03 21:53:39 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/h_minishell.h"
 
-#ifndef TEST
-
-int	main(int argc, char *argv[])
+int	ft_export(char **argv)
 {
-	(void)argc;
 	(void)argv;
-
-	// // pwdコマンド
-	// ft_pwd();
-	// return (0);
-
-	// // echoコマンド
-	// // normal case
-	// char *test_1[] = {"echo", "-n", "Hello", "World", NULL};
-	// ft_echo(test_1);
-	// // n_flag case
-	// char *test_2[] = {"echo", "-n", "-n", NULL};
-	// ft_echo(test_2);
-	// // complex case
-	// char *test_3[] = {"echo", "-n", "-n", "-nHello", "-n", NULL};
-	// ft_echo(test_3);
 	return (0);
 }
-#endif // TEST
