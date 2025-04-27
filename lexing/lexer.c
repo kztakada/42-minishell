3dfb61040b7e8e0c1e06d9ca6fce2be857039075
katakada <katakada@student.42tokyo.jp>
2025-04-27 21:04:04 +0900

    fix: handle after quote empty input case in lexicize_start_with_quote function

--- lexing/lexer.c
+++ lexing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 17:47:32 by katakada          #+#    #+#             */
-/*   Updated: 2025/04/27 17:37:11 by katakada         ###   ########.fr       */
+/*   Updated: 2025/04/27 20:53:07 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ static int	lexicize_start_with_quote(char *input, t_list **token_list)
 	if (put_size == FAILURE)
 		return (FAILURE);
 	ptr_input += put_size;
+	if (*ptr_input == '\0')
+		return (ptr_input - input);
 	put_size = lexicize_text_in_quote(ptr_input, token_list, quote);
 	if (put_size == FAILURE)
 		return (FAILURE);
