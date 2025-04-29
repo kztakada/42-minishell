37bc9916d60d19269e37332fa52d6e225cd7689f
42kharuya <haruya.0411.k@gmail.com>
2025-04-30 00:48:27 +0900

    fix: Update last modified date in exec_main.c and test_utils.c, and clean up commented code

--- exec/exec_main.c
+++ exec/exec_main.c
@@ -6,20 +6,30 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 14:55:16 by kharuya           #+#    #+#             */
-/*   Updated: 2025/04/19 15:18:55 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/04/30 00:23:53 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/h_minishell.h"
 
-// ft_exec_child
+// // exec_external
+// int main(int ac, char *av[], char *envp[])
+// {
+// 	(void)ac;
+// 	(void)av;
+// 	t_minishell minishell = minishell_init(envp);
+// 	t_node		*node = node_init();
+// 	exec_external(node, &minishell);
+// 	free (node->expanded_args);
+// 	free (node);
+// }
+
+// check_redirection
 int main(int ac, char *av[], char *envp[])
 {
 	(void)ac;
 	(void)av;
 	t_minishell minishell = minishell_init(envp);
 	t_node		*node = node_init();
-	ft_exec_child(t_node *node, t_minishell *minishell);
-	free (node->expanded_args);
-	free (node);
+	return (0);
 }
--- exec/test_utils.c
+++ exec/test_utils.c
@@ -6,12 +6,13 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:14:40 by kharuya           #+#    #+#             */
-/*   Updated: 2025/04/19 15:15:17 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/04/25 09:07:29 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/h_minishell.h"
 
+// exec_external(absolute path success)
 t_node	*node_init(void)
 {
 	t_node	*node;
@@ -27,3 +28,85 @@ t_node	*node_init(void)
 	node->right = NULL;
 	return (node);
 }
+
+// // exec_external(relative path success)
+// t_node	*node_init(void)
+// {
+// 	t_node	*node;
+// 	node = (t_node *)malloc(sizeof(t_node) * 1);
+// 	node->expanded_args = (char **)malloc(sizeof(char *) * (2 + 1));
+// 	node->expanded_args[0] = "ls";
+// 	node->expanded_args[1] = "-l";
+// 	node->expanded_args[2] = NULL;
+// 	node->type = N_CMD;
+// 	node->args = "ls -l";
+// 	node->io_list = NULL;
+// 	node->left = NULL;
+// 	node->right = NULL;
+// 	return (node);
+// }
+
+// // exec_external(absolute cmd not found)
+// t_node	*node_init(void)
+// {
+// 	t_node	*node;
+// 	node = (t_node *)malloc(sizeof(t_node) * 1);
+// 	node->expanded_args = (char **)malloc(sizeof(char *) * (1 + 1));
+// 	node->expanded_args[0] = "asd/dfa";
+// 	node->expanded_args[1] = NULL;
+// 	node->type = N_CMD;
+// 	node->args = "asd/dfa";
+// 	node->io_list = NULL;
+// 	node->left = NULL;
+// 	node->right = NULL;
+// 	return (node);
+// }
+
+// // exec_external(relative cmd not found)
+// t_node	*node_init(void)
+// {
+// 	t_node	*node;
+// 	node = (t_node *)malloc(sizeof(t_node) * 1);
+// 	node->expanded_args = (char **)malloc(sizeof(char *) * (1 + 1));
+// 	node->expanded_args[0] = "dfa";
+// 	node->expanded_args[1] = NULL;
+// 	node->type = N_CMD;
+// 	node->args = "dfa";
+// 	node->io_list = NULL;
+// 	node->left = NULL;
+// 	node->right = NULL;
+// 	return (node);
+// }
+
+// // exec_external(absolute permission denied)
+// t_node	*node_init(void)
+// {
+// 	t_node	*node;
+// 	node = (t_node *)malloc(sizeof(t_node) * 1);
+// 	node->expanded_args = (char **)malloc(sizeof(char *) * (1 + 1));
+// 	node->expanded_args[0] = "/Users/kharuya/test.sh";
+// 	node->expanded_args[1] = NULL;
+// 	node->type = N_CMD;
+// 	node->args = "/Users/kharuya/test.sh";
+// 	node->io_list = NULL;
+// 	node->left = NULL;
+// 	node->right = NULL;
+// 	return (node);
+// }
+
+// // exec_external(execve error)
+// t_node	*node_init(void)
+// {
+// 	t_node	*node;
+// 	node = (t_node *)malloc(sizeof(t_node) * 1);
+// 	node->expanded_args = (char **)malloc(sizeof(char *) * (2 + 1));
+// 	node->expanded_args[0] = "ls";
+// 	node->expanded_args[1] = "dfa";
+// 	node->expanded_args[2] = NULL;
+// 	node->type = N_CMD;
+// 	node->args = "ls dfa";
+// 	node->io_list = NULL;
+// 	node->left = NULL;
+// 	node->right = NULL;
+// 	return (node);
+// }
