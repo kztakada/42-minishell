7743176e932e0f5b63fb90a2888346f693916fe4
42kharuya <haruya.0411.k@gmail.com>
2025-04-30 00:32:03 +0900

    refactor: Improve error message handling in err_msg.c

--- exec/err_msg.c
+++ exec/err_msg.c
@@ -6,14 +6,53 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 15:56:37 by kharuya           #+#    #+#             */
-/*   Updated: 2025/04/20 17:14:52 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/04/25 07:05:35 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/h_minishell.h"
 
-int	ft_err_msg(t_err err)
+static int	msg_cmd_not_found(t_err err)
 {
-	(void)err;
-	return ;
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(err.cause, 2);
+	ft_putstr_fd(": command not found\n", 2);
+	return (err.no);
+}
+
+static int	msg_no_such_file(t_err err)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(err.cause, 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
+	return (err.no);
+}
+
+static int	msg_perm_denied(t_err err)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(err.cause, 2);
+	ft_putstr_fd(": Permission denied\n", 2);
+	return (err.no);
+}
+
+static int msg_ambiguous(t_err err)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(err.cause, 2);
+	ft_putstr_fd(": ambiguous redirect\n", 2);
+	return (err.no);
+}
+
+int	err_msg(t_err err)
+{
+	if (err.msg == ERRMSG_CMD_NOT_FOUND)
+		return (msg_cmd_not_found(err));
+	else if (err.msg == ERRMSG_NO_SUCH_FILE)
+		return (msg_no_such_file(err));
+	else if (err.msg == ERRMSG_PERM_DENIED)
+		return (msg_perm_denied(err));
+	else if (err.msg == ERRMSG_AMBIGUOUS)
+		return (msg_ambiguous(err));
+	return (0);
 }
