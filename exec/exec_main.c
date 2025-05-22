3ba8b283d015c34caba89fd87d9d60238f436e80
42kharuya <haruya.0411.k@gmail.com>
2025-05-22 13:33:52 +0900

    refactor: Update command execution flow and environment handling in exec functions

--- exec/exec_main.c
+++ exec/exec_main.c
@@ -6,33 +6,23 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 14:55:16 by kharuya           #+#    #+#             */
-/*   Updated: 2025/05/16 11:53:34 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/05/20 04:16:34 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/h_minishell.h"
 
-// // exec_cmd_external
-// int main(int ac, char *av[], char *envp[])
-// {
-// 	(void)ac;
-// 	(void)av;
-// 	t_minishell minishell = minishell_init(envp);
-// 	t_abs_node		*node = abs_init();
-// 	exec_cmd_external(node, &minishell);
-// 	free (node->expanded_args);
-// 	free (node);
-// }
-
-// check_redirection
+// check_redirection input (external)
 int main(int ac, char *av[], char *envp[])
 {
 	(void)ac;
 	(void)av;
 	t_abs_node		*abs = abs_init();
-	t_minishell minishell = minishell_init(envp);
-	if (abs->redirection_list)
-		exec_redirection(abs);
-	exec_cmd_builtin(abs->expanded_args, &minishell);
+	t_env	*env = (t_env *)malloc(sizeof(t_env) * 1);
+	env->env_lists = init_envlst(envp);
+	env->envp = envp;
+	env->exit_status = 0;
+	exec_redirection(abs);
+	exec_cmd_builtin(abs->expanded_args, env);
 	return (0);
 }
