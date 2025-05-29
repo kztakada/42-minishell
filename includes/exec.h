1876fb9b6c64b7b0d0d54dd0fdabd09bbf7020e6
42kharuya <haruya.0411.k@gmail.com>
2025-05-29 22:16:09 +0900

    fix: Update author information and improve structure in exec.h

--- includes/exec.h
+++ includes/exec.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 00:13:37 by katakada          #+#    #+#             */
-/*   Updated: 2025/05/23 01:35:25 by katakada         ###   ########.fr       */
+/*   Updated: 2025/05/28 16:12:21 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,20 @@
 
 # include "minishell.h"
 
+// pipe access mode
+typedef enum e_pipe_access_mode
+{
+	PIPE_WRITE_MODE,
+	PIPE_READ_MODE,
+}				t_pipe_access_mode;
+
+
 // This is saved stdout and stdin
 typedef struct s_std
 {
 	int			saved_stdin;
 	int			saved_stdout;
-}				t_std;
+}				t_saved_std;
 typedef enum e_err_msg
 {
 	NONE,
@@ -42,7 +50,7 @@ typedef struct s_path
 	char		*path;
 }				t_path;
 
-// err_msg (lib_func)
+// err_msg (std_lib_func)
 int				err_msg_malloc(void);
 
 // err_msg (redirection)
@@ -57,16 +65,16 @@ int				msg_cmd_not_found(t_err err);
 int				msg_is_directory(t_err err);
 
 // prototypes exec
-int				exec_cmd(t_abs_node *abs, t_env *env, t_std std);
-int				exec_cmd_external(t_abs_node *node, char *envp[]);
+int				exec_abs(t_abs_node *abs_tree, t_env *env, t_saved_std std, t_bool piped);
+int				exec_cmd(t_abs_node *abs_tree, t_env *env, t_saved_std std, t_bool piped);
+int				exec_cmd_external(t_abs_node *abs_tree, char *envp[]);
 int				exec_cmd_builtin(char **args, t_env *env);
-int				exec_redirection(t_abs_node *node);
+int				exec_redirection(t_list	*redirection_list);
+int				exec_pipe(t_abs_node *abs_tree, t_env *env, t_saved_std std);
+int				get_exit_status(int status);
 t_path			get_path(char *cmd);
 t_path			create_t_path(char *cmd, int err_exit_s, int err_msg,
 					char *err_cause);
 t_err			create_t_err(int err_exit_s, int err_msg, char *err_cause);
-
-// test_utils (exec)
-t_abs_node		*abs_init(void);
 
 #endif
