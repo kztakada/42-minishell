6a1b416a34f95027ef3d0b810a8062b94cda180c
42kharuya <haruya.0411.k@gmail.com>
2025-06-02 04:56:33 +0900

    fix: Refactor save_std function to encapsulate stdin and stdout duplication

--- exec/exec.c
+++ exec/exec.c
@@ -6,11 +6,20 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 00:13:08 by katakada          #+#    #+#             */
-/*   Updated: 2025/05/28 16:28:39 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/05/30 01:23:38 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+static t_saved_std	save_std(void)
+{
+	t_saved_std	std;
+
+	std.saved_stdin = dup(0);
+	std.saved_stdout = dup(1);
+	return (std);
+}
 
 void	exec(t_abs_node *abs_tree, t_env *env)
 {
@@ -23,8 +32,7 @@ void	exec(t_abs_node *abs_tree, t_env *env)
 		*(env->exit_status) = result;
 		return ;
 	}
-	std.saved_stdin = dup(0);
-	std.saved_stdout = dup(1);
+	std = save_std();
 	result = exec_abs(abs_tree, env, std, FALSE);
 	*(env->exit_status) = result;
 	return ;
