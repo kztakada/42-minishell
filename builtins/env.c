2d153edc6c7dc9663532325fcf0a88a58d2c5292
42kharuya <haruya.0411.k@gmail.com>
2025-06-02 04:52:12 +0900

    feat: Add environment utility functions for managing environment variables

--- builtins/env.c
+++ builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 21:53:19 by kharuya           #+#    #+#             */
-/*   Updated: 2025/05/28 17:16:31 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/05/30 20:07:11 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_env(t_list *env_list)
 {
 	t_env_var	*env;
 
+	if (!is_env_exist(env_list, "PATH"))
+		return (env_err_msg_path_env());
 	while (env_list)
 	{
 		env = (t_env_var *)env_list->content;
