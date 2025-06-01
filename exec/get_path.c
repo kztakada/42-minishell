05a5a5b9af667b0fa8e0af423eeca57785dd1849
42kharuya <haruya.0411.k@gmail.com>
2025-06-02 04:56:40 +0900

    fix: Update get_path function to use environment variables for PATH and correct return values

--- exec/get_path.c
+++ exec/get_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_path.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 17:51:35 by kharuya           #+#    #+#             */
-/*   Updated: 2025/05/23 01:30:23 by katakada         ###   ########.fr       */
+/*   Updated: 2025/05/30 17:53:21 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static t_path	check_exec(char *path, char *cmd)
 	if ((access(path, X_OK) == 0))
 		return (create_t_path(path, EXIT_S_SUCCESS, NONE, NULL));
 	else
-		return (create_t_path(NULL, EXIT_S_CMD_FAILURE,
+		return (create_t_path(cmd, EXIT_S_CMD_FAILURE,
 				ERRMSG_PERM_DENIED, cmd));
 }
 
@@ -26,17 +26,19 @@ static t_path	get_absolute(char *cmd)
 	if (access(cmd, F_OK) == 0)
 		return (check_exec(cmd, cmd));
 	else
-		return (create_t_path(NULL, EXIT_S_CMD_NOT_FOUND,
+		return (create_t_path(cmd, EXIT_S_CMD_NOT_FOUND,
 				ERRMSG_CMD_NOT_FOUND, cmd));
 }
 
-static t_path	get_relative(char *cmd)
+static t_path	get_relative(char *cmd, t_list *env_vars)
 {
+	char 	*path_env_value;
 	char	**split_path_env;
 	char	*tmp_path;
 	int		i;
 
-	split_path_env = ft_split(getenv("PATH"), ':');
+	path_env_value = get_env_value(env_vars, "PATH");
+	split_path_env = ft_split(path_env_value, ':');
 	i = -1;
 	while (split_path_env[++i])
 	{
@@ -46,14 +48,19 @@ static t_path	get_relative(char *cmd)
 		free(tmp_path);
 	}
 	ft_free_char2(split_path_env);
-	return (create_t_path(NULL, EXIT_S_CMD_NOT_FOUND,
+	return (create_t_path(cmd, EXIT_S_CMD_NOT_FOUND,
 			ERRMSG_CMD_NOT_FOUND, cmd));
 }
 
-t_path	get_path(char *cmd)
+t_path	get_path(char *cmd, t_list *env_vars)
 {
-	if (ft_strchr(cmd, '/'))
-		return (get_absolute(cmd));
+	if (is_env_exist(env_vars, "PATH"))
+	{
+		if (ft_strchr(cmd, '/'))
+			return (get_absolute(cmd));
+		else
+			return (get_relative(cmd, env_vars));
+	}
 	else
-		return (get_relative(cmd));
+		return (create_t_path(cmd, EXIT_S_CMD_NOT_FOUND, ERRMSG_NO_SUCH_FILE, cmd));
 }
