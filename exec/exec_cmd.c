ace856ca87fee75859ee426c45db182da16bbaff
42kharuya <haruya.0411.k@gmail.com>
2025-06-04 15:25:27 +0900

    fix: Correct pointer usage in reset_saved_stds and exec_cmd functions

--- exec/exec_cmd.c
+++ exec/exec_cmd.c
@@ -6,18 +6,18 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 01:17:57 by kharuya           #+#    #+#             */
-/*   Updated: 2025/05/30 15:03:58 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/06/02 05:34:09 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-static void	reset_saved_stds(t_saved_std std, t_bool piped)
+static void	reset_saved_stds(t_saved_std *std, t_bool piped)
 {
 	if (piped == TRUE)
 		return ;
-	dup2(std.saved_stdin, 0);
-	dup2(std.saved_stdout, 1);
+	dup2(std->saved_stdin, 0);
+	dup2(std->saved_stdout, 1);
 	return ;
 }
 
@@ -36,7 +36,7 @@ static t_bool	is_builtin(char *cmd)
 	return (FALSE);
 }
 
-int	exec_cmd(t_abs_node *abs_tree, t_env *env, t_saved_std std, t_bool piped)
+int	exec_cmd(t_abs_node *abs_tree, t_env *env, t_saved_std *std, t_bool piped)
 {
 	int	status;
 
--- exec/exec_cmd_external.c
+++ exec/exec_cmd_external.c
@@ -6,7 +6,7 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 03:59:24 by kharuya           #+#    #+#             */
-/*   Updated: 2025/06/02 04:53:02 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/06/02 05:36:03 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	exec_cmd_external(t_abs_node *abs_tree, t_list *env_vars)
 	int		status;
 	pid_t	pid;
 	t_path	path;
-	char 	**envp;
+	char	**envp;
 
 	// シグナルの処理は後回し
 	pid = fork();
@@ -26,7 +26,7 @@ int	exec_cmd_external(t_abs_node *abs_tree, t_list *env_vars)
 		status = exec_redirection(abs_tree->redirections);
 		if (status != EXIT_S_SUCCESS)
 			exit(status);
-		path = get_path(abs_tree->expanded_args[0],env_vars);
+		path = get_path(abs_tree->expanded_args[0], env_vars);
 		if (path.err.exit_s != EXIT_S_SUCCESS)
 			exit(err_msg_external(path.err));
 		envp = convert_list_to_envp(env_vars);
