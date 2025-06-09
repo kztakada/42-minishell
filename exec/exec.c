c89f4eb6e187e856bac36c655969bca3989e5665
42kharuya <haruya.0411.k@gmail.com>
2025-06-10 00:59:37 +0900

    fix: Update exec and exec_subshell functions for improved process handling and signal management

--- exec/exec.c
+++ exec/exec.c
@@ -6,11 +6,12 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 00:13:08 by katakada          #+#    #+#             */
-/*   Updated: 2025/06/08 06:59:04 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/06/09 18:43:03 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+#include "for_test_print.h"
 
 void	exec(t_abs_node *abs_tree, t_env *env)
 {
@@ -24,6 +25,7 @@ void	exec(t_abs_node *abs_tree, t_env *env)
 		return ;
 	}
 	std = save_stds();
+	print_abs_tree(abs_tree);
 	result = exec_abs(abs_tree, env, &std, FALSE);
 	*(env->exit_status) = result;
 	return ;
--- exec/exec_subshell.c
+++ exec/exec_subshell.c
@@ -6,19 +6,31 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 06:14:11 by kharuya           #+#    #+#             */
-/*   Updated: 2025/06/08 08:09:26 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/06/09 20:50:50 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+#include "signal_for_minishell.h"
 
 int	exec_subshell(t_abs_node *abs_tree, t_env *env, t_saved_std *std, t_bool piped)
 {
-	int	status;
+	int		status;
+	pid_t	pid;
 
-	status = exec_redirection(abs_tree->redirections);
-	if (status != EXIT_S_SUCCESS)
-		return (status);
-	status = exec_abs(abs_tree->left, env, std, piped);
-	return (reset_stds(std, piped), status);
+	pid = fork();
+	if (pid == -1)
+		return (perror(ERROR_FORK), EXIT_S_FAILURE);
+	if (pid == 0)
+	{
+		set_sig_handlers_in_exec_child();
+		status = exec_redirection(abs_tree->redirections);
+		if (status != EXIT_S_SUCCESS)
+			return (status);
+		status = exec_abs(abs_tree->left, env, std, piped);
+		exit(status);
+	}
+	set_sig_handlers_in_exec_parent();
+	waitpid(pid, &status, 0);
+	return (get_exit_status(status));
 }
