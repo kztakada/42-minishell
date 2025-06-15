7b28dbd6bc2da262b414300c4fade7551db74fec
42kharuya <haruya.0411.k@gmail.com>
2025-06-15 12:50:50 +0900

    fix: update author information and correct function signatures in builtins.h

--- includes/builtins.h
+++ includes/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 01:32:47 by katakada          #+#    #+#             */
-/*   Updated: 2025/06/15 02:20:51 by katakada         ###   ########.fr       */
+/*   Updated: 2025/06/15 07:52:15 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 
 // err_msg (builtin)
 int		env_err_msg_path_env(void);
-int		cd_err_msg_file(char *path);
+int		cd_err_msg_no_file(char *path);
 int		cd_err_msg_permission(char *path);
 int		cd_err_msg_home(void);
 int		cd_err_msg_args(void);
+int		cd_err_msg_is_file(char *path);
 int		export_err_msg(char *arg);
 int		unset_err_msg(char *arg);
 int		exit_err_msg_digit(char *arg);
@@ -31,7 +32,7 @@ int		ft_cd(char **args, t_list *env_list, t_bool *unset_oldpwd);
 int		ft_echo(char **args);
 int		ft_env(t_list *env_list);
 int		ft_export(char **argv, t_list *env_list, t_bool is_interactive);
-int		ft_pwd(void);
+int		ft_pwd(t_list *env_list);
 int		ft_unset(char **args, t_list *env_list, t_bool *unset_oldpwd);
 int		check_name_error(char *arg);
 void	ft_exit(char **args, t_env *env, t_exit_status exit_s);
