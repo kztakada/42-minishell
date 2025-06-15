3decf4837090fef8c88801822eccd9fd166310e2
42kharuya <haruya.0411.k@gmail.com>
2025-06-15 16:34:21 +0900

    fix: improve error handling in ft_pwd by adding a dedicated error message for unset PWD

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,22 +6,33 @@
 /*   By: kharuya <haruya.0411.k@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 21:53:41 by kharuya           #+#    #+#             */
-/*   Updated: 2025/06/15 13:07:36 by kharuya          ###   ########.fr       */
+/*   Updated: 2025/06/15 16:31:58 by kharuya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/builtins.h"
 
+static int	pwd_err_msg_pwd(void)
+{
+	ft_putstr_fd("minishell: pwd: PWD not set\n", STDERR_FILENO);
+	return (EXIT_S_FAILURE);
+}
+
 int	ft_pwd(t_list *env_list)
 {
 	char	*path;
 
-	path = ft_strdup(get_env_value(env_list, "PWD"));
+	path = getcwd(NULL, 0);
 	if (!path)
 	{
-		path = getcwd(NULL, 0);
-		if (!path)
-			return (perror(ERROR_GETCWD), EXIT_S_FAILURE);
+		if (is_env_exist(env_list, "PWD"))
+		{
+			path = ft_strdup(get_env_value(env_list, "PWD"));
+			if (!path)
+				return (perror(ERROR_GETCWD), EXIT_S_FAILURE);
+		}
+		else
+			return (pwd_err_msg_pwd());
 	}
 	ft_putstr_fd(path, STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
